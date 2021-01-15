6a4ab476e95ba3730784d3c200f731eb9a2e4e84
Bilin Tsui <xuminruixuan@live.cn>
2021-01-15 20:32:55 +0800

    About license
    Removed descriptions for GNU GPL v3 in every code file's header, which may be confusing to everyone.

--- mod/network.h
+++ mod/network.h
@@ -5,8 +5,8 @@
 	Minecraft Relay Server, version 1.1.3
 	Copyright (c) 2020-2021 Bilin Tsui. All right reserved.
 	This is a Free Software, absolutely no warranty.
+
 	Licensed with GNU General Public License Version 3 (GNU GPL v3).
-	It basically means you have free rights for uncommerical use and modify, also restricted you to comply the license, whether part of original release or modified part by you.
 	For detailed license text, watch: https://www.gnu.org/licenses/gpl-3.0.html
 */
 #ifndef _MOD_NETWORK_H_
