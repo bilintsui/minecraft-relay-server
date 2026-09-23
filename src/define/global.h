4a4f3c003357447ecbc9d211801c0ec7608d9293
Bilin Tsui <xuminruixuan@live.cn>
2026-09-24 00:59:39 +0800

    chore: bump version to 1.2.2

--- src/define/global.h
+++ src/define/global.h
@@ -23,8 +23,8 @@
 #endif
 
 /* program metadata */
-#define MCRELAY_VERSION_DISPLAY	"1.2.1"
-#define MCRELAY_VERSION_INTERNAL	"78"
+#define MCRELAY_VERSION_DISPLAY	"1.2.2"
+#define MCRELAY_VERSION_INTERNAL	"79"
 #define MCRELAY_COPYYEAR	"2020-2026"
 
 /* protocol version constant */
